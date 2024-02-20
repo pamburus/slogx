f0c963f9563a2e52085ad690799b50b8307d98b5
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2024-02-20 21:22:26 +0100

    draft

--- ansitty/ansitty.go
+++ ansitty/ansitty.go
@@ -7,8 +7,8 @@ import (
 )
 
 // Enable enables possibility to use ANSI escape sequences in TTY if possible.
-func Enable(stream io.Writer) bool {
-	if f, ok := stream.(*os.File); ok {
+func Enable(writer io.Writer) bool {
+	if f, ok := writer.(*os.File); ok {
 		return setEnabled(f.Fd(), true)
 	}
 
@@ -16,10 +16,32 @@ func Enable(stream io.Writer) bool {
 }
 
 // Disable disables possibility to use ANSI escape sequences in TTY if possible.
-func Disable(stream io.Writer) bool {
-	if f, ok := stream.(*os.File); ok {
+func Disable(writer io.Writer) bool {
+	if f, ok := writer.(*os.File); ok {
 		return setEnabled(f.Fd(), false)
 	}
 
 	return false
+}
+
+// Writer is a convenience function to enable possibility to use ANSI escape sequences in TTY if possible to writer.
+// It calls [Enable] on the given writer and returns the same writer.
+func Writer(writer io.Writer) io.Writer {
+	Enable(writer)
+
+	return writer
+}
+
+// Stdout enables possibility to use ANSI escape sequences in TTY if possible to stdout and returns os.Stdout.
+func Stdout() *os.File {
+	Enable(os.Stdout)
+
+	return os.Stdout
+}
+
+// Stderr enables possibility to use ANSI escape sequences in TTY if possible to stderr and returns os.Stderr.
+func Stderr() *os.File {
+	Enable(os.Stderr)
+
+	return os.Stderr
 }
